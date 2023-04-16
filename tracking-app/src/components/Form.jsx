c576a8f47821799748d3c9390b75d419f12a7ffc
acu8 <ewatanabe786@gmail.com>
2023-04-16 13:06:31 +0900

    additional function

---
+++ tracking-app/src/components/Form.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import uuid from "react-uuid";
+
+const Form = ({ todos, setTodos }) => {
+  const [inputText, setInputText] = useState("");
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        id: uuid(),
+        text: inputText,
+        completed: false,
+      },
+    ]);
+    setInputText("");
+  };
+
+  return (
+    <div className="title">
+      <header>
+        <h1>Tracking App</h1>
+      </header>
+      <form>
+        <input
+          onChange={handleChange}
+          type="text"
+          className="todo-input"
+          value={inputText}
+        />
+        <button type="submit" onClick={handleInput}>
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
